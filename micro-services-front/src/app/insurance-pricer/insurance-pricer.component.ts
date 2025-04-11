1f5d75e0c05248f032eba4c4147690b6ab4b8b01
Rolando Mesa <rolandomesagdp@gmail.com>
2025-04-11 12:31:19 +0200

    Front connected to the dockerized back

--- micro-services-front/src/app/insurance-pricer/insurance-pricer.component.ts
+++ micro-services-front/src/app/insurance-pricer/insurance-pricer.component.ts
@@ -3,27 +3,32 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Driver } from '../insurance/driver.interface';
 import { HttpClient } from '@angular/common/http';
 import { InsuranceOffer } from '../insurance/insurance-offer.interface';
 import { tap } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-insurance-pricer',
   imports: [
+    CommonModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   templateUrl: './insurance-pricer.component.html',
   styleUrl: './insurance-pricer.component.scss'
 })
 export class InsurancePricerComponent {
   driverForm: FormGroup;
+  offer: InsuranceOffer | null = null;
 
   constructor (private formBuilder: FormBuilder, private httpClient: HttpClient) {
     this.driverForm = this.formBuilder.group({
@@ -39,8 +44,12 @@ export class InsurancePricerComponent {
     const url: string = `${environment.insurancePricerBaseUrl}/InsurancePrice`;
     this.httpClient.post<InsuranceOffer>(url, driver).pipe(
       tap((offer: InsuranceOffer) => {
-        console.log(offer);
+        this.offer = { ...offer };
       })
     ).subscribe();
+  }
+
+  clearOffer(): void {
+    this.offer = null;
   }
 }
